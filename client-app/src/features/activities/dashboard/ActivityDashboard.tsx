b395251f872bd9835f4cc235fec4546748271da3
Bipal Goyal <goyalbl@mail.uc.edu>
2024-04-11 19:48:32 -0400

    added axios requests for CRUD operations for activities

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,14 +14,15 @@ interface Props {
     closeForm: () => void
     createOrEdit: (activity: Activity) => void
     deleteActivity: (id: string) => void
+    submitting: boolean
 }
 
-export default function ActvityDashboard({activities, selectedActivity,
+export default function ActvityDashboard({activities, selectedActivity, submitting,
             selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity}/>
+                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} submitting={submitting}/>
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
@@ -31,7 +32,12 @@ export default function ActvityDashboard({activities, selectedActivity,
                     openForm={openForm}
                 />}
                 {editMode && 
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+                <ActivityForm 
+                    closeForm={closeForm} 
+                    activity={selectedActivity} 
+                    createOrEdit={createOrEdit}
+                    submitting={submitting}
+                />}
             </Grid.Column>
         </Grid>
     )
